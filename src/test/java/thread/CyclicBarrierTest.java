8b55d2cad232ecb8ac47ac547705840afe86f754
董晓斌 <dongxiaobin@wondersoft.cn>
2019-01-14 15:05:56 +0800

    格式化代码

--- src/test/java/thread/CyclicBarrierTest.java
+++ src/test/java/thread/CyclicBarrierTest.java
@@ -7,16 +7,74 @@ package thread;
 
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class CyclicBarrierTest
 {
 
+    public static void main(String[] args)
+        throws InterruptedException, BrokenBarrierException
+    {
+        final int N = 5;
+        Thread[] allSoldier = new Thread[N];
+        boolean flag = false;
+        CyclicBarrier cyclic = new CyclicBarrier(N, new BarrierRun(flag, N));
+        for (int i = 0; i < N; i++ )
+        {
+            allSoldier[i] = new Thread(new Soldier("士兵" + i, cyclic));
+            allSoldier[i].start();
+        }
+        System.out.println("-----------");
+    }
+
+    public static void main2(String[] args)
+    {
+        ExecutorService service = Executors.newCachedThreadPool();
+        final CyclicBarrier cb = new CyclicBarrier(3);// 创建CyclicBarrier对象并设置3个公共屏障点
+        for (int i = 0; i < 3; i++ )
+        {
+            Runnable runnable = new Runnable()
+            {
+                public void run()
+                {
+                    try
+                    {
+                        Thread.sleep((long)(Math.random() * 10000));
+                        System.out.println("线程" + Thread.currentThread().getName()
+                                           + "即将到达集合地点1，当前已有" + cb.getNumberWaiting()
+                                           + "个已经到达，正在等候");
+                        cb.await();// 到此如果没有达到公共屏障点，则该线程处于等待状态，如果达到公共屏障点则所有处于等待的线程都继续往下运行
+
+                        Thread.sleep((long)(Math.random() * 10000));
+                        System.out.println("线程" + Thread.currentThread().getName()
+                                           + "即将到达集合地点2，当前已有" + cb.getNumberWaiting()
+                                           + "个已经到达，正在等候");
+                        cb.await();
+                        Thread.sleep((long)(Math.random() * 10000));
+                        System.out.println("线程" + Thread.currentThread().getName()
+                                           + "即将到达集合地点3，当前已有" + cb.getNumberWaiting()
+                                           + "个已经到达，正在等候");
+                        cb.await();
+                    }
+                    catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            System.out.println("--------------------------");
+            service.execute(runnable);
+        }
+        service.shutdown();
+    }
+
     public static class Soldier implements Runnable
     {
-        private String soldier;
-
         private final CyclicBarrier cyclic;
+
+        private String soldier;
 
         Soldier(String name, CyclicBarrier _cyclic)
         {
@@ -65,20 +123,5 @@ public class CyclicBarrierTest
                 flag = true;
             }
         }
-    }
-
-    public static void main(String[] args)
-        throws InterruptedException, BrokenBarrierException
-    {
-        final int N = 5;
-        Thread[] allSoldier = new Thread[N];
-        boolean flag = false;
-        CyclicBarrier cyclic = new CyclicBarrier(N, new BarrierRun(flag, N));
-        for (int i = 0; i < N; i++ )
-        {
-            allSoldier[i] = new Thread(new Soldier("士兵" + i, cyclic));
-            allSoldier[i].start();
-        }
-        System.out.println("-----------");
     }
 }
